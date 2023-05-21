3390ef0b5266399a6d8e72d2ece87f1bb2c94398
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-05-21 22:10:59 +0300

    searchBST() implemented in Java🔥| Runtime: 100% (0ms), Memory:  8.59% (44.3mb) 🚀

---
+++ Algorithms/0700-Search-in-a-Binary-Search-Tree/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null || val == root.val)
+            return root;
+
+        return (root == null || val == root.val) ? root :
+        
+        return searchBST((val < root.val) ? (root.left) : (root.right), val);
+    }
+}
