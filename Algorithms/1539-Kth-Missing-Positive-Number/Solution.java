443df077b2f101a573b07f961bb852d0f7266995
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-05-21 21:52:23 +0300

    findKthPositive() implemented in Java 🔥| Runtime: 100% (0ms), Memory: 5.8% (42.9mb) 🚀

---
+++ Algorithms/1539-Kth-Missing-Positive-Number/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int low = 0, high = arr.length - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(arr[mid] - (mid + 1) < k)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return (low + k);
+    }
+}
