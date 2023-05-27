04a74e5fa34b79aba76073f0875b275717661053
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-05-27 03:16:41 +0300

    Refactored pivotIndex() ✅

--- Algorithms/0724-Find-Pivot-Index/Solution.java
+++ Algorithms/0724-Find-Pivot-Index/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int total = 0, left = 0;
+        int N = nums.length, totalSum = 0, leftSum = 0;
+
+        for(int i = 0; i < N; i++)
+            totalSum += nums[i];
         
-        for(int element : nums)
-            total += element;
-        
-        for(int i = 0; i < nums.length; left += nums[i++])
-            if(left * 2 == total - nums[i])
+        for(int i = 0; i < N; leftSum += nums[i++])
+            if(leftSum * 2 == totalSum - nums[i])
                 return i;
         
         return -1;
