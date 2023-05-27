99677659a6a92543a5fc979db848815cc30db356
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-05-27 03:25:21 +0300

    increasingTriplet() implemented in Java 🪝 | Time Complexity: O(N) 🚀

---
+++ Algorithms/0334-Increasing-Triplet-Subsequence/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int N = nums.length, min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        for(int i = 0; i < N; i++) 
+            if(nums[i] <= min1)
+                min1 = nums[i];
+            else if(nums[i] <= min2)
+                min2 = nums[i];
+            else
+                return true;
+        
+
+        return false;
+    }
+}
