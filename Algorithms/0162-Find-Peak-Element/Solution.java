7eebc284169db18ec785cef365d83c3f02275fd2
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-06-27 14:26:36 +0300

    findPeakElement() implemented in Java 🪝 | BSA, TC: O(log N) | Runtime: 100% (0ms), Memory: 41.36% (41.8mb) 🚀

---
+++ Algorithms/0162-Find-Peak-Element/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int N = nums.length;
+        if(N == 1)
+            return 0;
+        
+        if(nums[0] > nums[1])
+            return 0;
+        if(nums[N - 1] > nums[N - 2])
+            return N - 1;
+        
+        int low = 1, high = N - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
+                return mid;
+            
+            else if(nums[mid - 1] > nums[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return 0;
+    }
+}
