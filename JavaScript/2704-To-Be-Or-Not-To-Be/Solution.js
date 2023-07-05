12da48b521d5b73dda7f5ca9b3a87ce7cb391b3b
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-07-05 19:14:11 +0300

    expect() implemented in JavaScript ✨ | Runtime: 22.76% (64ms), Memory: 63.47% (41.7mb)  🚀

---
+++ JavaScript/2704-To-Be-Or-Not-To-Be/Solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (el) => (el === val) ? true : (() => {
+            throw new Error("Not Equal");
+        })(),
+        notToBe: (el) => (el !== val) ? true : (() => {
+            throw new Error("Equal");
+        })()
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
