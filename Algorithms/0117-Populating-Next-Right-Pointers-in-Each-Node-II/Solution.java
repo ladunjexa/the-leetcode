ddd3bf3a8863632808e793a102e35edb8dd8d076
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-07-15 10:17:23 +0300

    connect() implemented in Java 🪝| Runtime: 48.66% (2ms), Memory: 59.6% (43.4mb) 🚀

---
+++ Algorithms/0117-Populating-Next-Right-Pointers-in-Each-Node-II/Solution.java
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+
+        if(root == null)
+            return null;
+        
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for(int i = 0; i < levelSize; i++) {
+                Node node = queue.remove();
+
+                node.next = (i == levelSize - 1) ? null : queue.peek();
+
+                if(node.left != null)
+                    queue.add(node.left);
+                
+                if(node.right != null)
+                    queue.add(node.right);
+            }
+        }
+
+        return root;
+    }
+}
