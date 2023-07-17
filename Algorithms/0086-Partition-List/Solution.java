93622674291449e0953d0caf31b191956825bf9a
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-07-17 06:29:12 +0300

    partition() implemented in Java 🪝| Runtime: 100% (0 ms), Memory: 13.61% (41.4 mb) 🚀

---
+++ Algorithms/0086-Partition-List/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode beforeHead = new ListNode(-1), afterHead = new ListNode(-1), before = beforeHead, after = afterHead;
+
+        while(head != null) {
+            if(head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+
+            head = head.next;
+        }
+
+        before.next = afterHead.next;
+        after.next = null;
+
+        return beforeHead.next;
+    }
+}
